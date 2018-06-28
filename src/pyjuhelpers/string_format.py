82c467c5bc45d288fcb0160367fa69bdb985a801
jumbrich <jueumb@gmail.com>
2018-06-28 16:55:27 +0200

    added config, formatter, module_import

---
+++ src/pyjuhelpers/string_format.py
@@ -0,0 +1,9 @@
+import string
+
+
+def reindent(s, numSpaces, prefix):
+    _prefix = "\n"
+    if prefix:
+        _prefix+=prefix
+    str = _prefix.join((numSpaces * " ") + i for i in s.splitlines())
+    return str
